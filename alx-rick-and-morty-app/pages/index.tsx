93753241ba499e0fea794c4f9935b1a9e7ec39a1
habyarimana caleb <callebhabyar55@gmail.com>
2025-08-20 21:56:19 +0200

    test error output to home page

--- alx-rick-and-morty-app/pages/index.tsx
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -3,6 +3,8 @@ import { GET_EPISODES } from "@/graphql/queries"
 import { EpisodeProps } from "@/interfaces"
 import { useEffect, useState } from "react"
 import EpisodeCard from "@/components/common/EpisodeCard"
+import ErrorBoundary from "@/components/ErrorBoundary"
+import ErrorProneComponent from "@/components/ErrorProneComponent"
 
 
 
@@ -26,6 +28,10 @@ const Home: React.FC = () => {
   const info = data?.episodes.info
 
   return (
+    <>
+    <ErrorBoundary>
+      <ErrorProneComponent />
+    </ErrorBoundary>
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#A3D5E0] to-[#F4F4F4] text-gray-800">
       {/* Header */}
       <header className="bg-[#4CA1AF] text-white py-6 text-center shadow-md">
@@ -67,6 +73,7 @@ const Home: React.FC = () => {
         <p>&copy; 2024 Rick and Morty Fan Page</p>
       </footer>
     </div>
+    </>
   )
 }
 
